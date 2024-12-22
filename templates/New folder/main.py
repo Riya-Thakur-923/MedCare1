21a99c848c51c6730b02febb40a952ca35983b0e
Riya Thakur <162443519+Riya-Thakur-923@users.noreply.github.com>
2024-12-22 20:48:41 +0530

    Update main.py

--- templates/New folder/main.py
+++ templates/New folder/main.py
@@ -4,6 +4,10 @@
 
 
 
+
+
+
+
 from flask import Flask, request, render_template, jsonify  # type: ignore # Import jsonify
 import numpy as np # type: ignore
 import pandas as pd # type: ignore
@@ -75,34 +79,68 @@ def index():
 # Define a route for the home page
 #The /predict route handles the form submission from the home page.
 
+
+
+
+
+
 @app.route('/predict', methods=['GET', 'POST'])
-def home():
+def predict():
     if request.method == 'POST':
         symptoms = request.form.get('symptoms')
-        # mysysms = request.form.get('mysysms')
-        # print(mysysms)
         print(symptoms)
-        if symptoms =="Symptoms":
+        
+        # Process the symptoms
+        data = {'message': 'Prediction successful', 'symptoms': symptoms}
+        print(data)
+        
+        if symptoms == "Symptoms":
             message = "Please either write symptoms or you have written misspelled symptoms"
             return render_template('index.html', message=message)
         else:
-
             # Split the user's input into a list of symptoms (assuming they are comma-separated)
             user_symptoms = [s.strip() for s in symptoms.split(',')]
             # Remove any extra characters, if any
             user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
             predicted_disease = get_predicted_value(user_symptoms)
             dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
-
             my_precautions = []
             for i in precautions[0]:
                 my_precautions.append(i)
-
-            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des,
-                                   my_precautions=my_precautions, medications=medications, my_diet=rec_diet,
-                                   workout=workout)
-
+            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des, my_precautions=my_precautions, medications=medications, my_diet=rec_diet, workout=workout)
     return render_template('index.html')
+
+
+
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'POST':
+#         symptoms = request.form.get('symptoms')
+#         # mysysms = request.form.get('mysysms')
+#         # print(mysysms)
+#         print(symptoms)
+#         if symptoms =="Symptoms":
+#             message = "Please either write symptoms or you have written misspelled symptoms"
+#             return render_template('index.html', message=message)
+#         else:
+
+#             # Split the user's input into a list of symptoms (assuming they are comma-separated)
+#             user_symptoms = [s.strip() for s in symptoms.split(',')]
+#             # Remove any extra characters, if any
+#             user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
+#             predicted_disease = get_predicted_value(user_symptoms)
+#             dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
+
+#             my_precautions = []
+#             for i in precautions[0]:
+#                 my_precautions.append(i)
+
+#             return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des,
+#                                    my_precautions=my_precautions, medications=medications, my_diet=rec_diet,
+#                                    workout=workout)
+
+#     return render_template('index.html')
 
 
 
